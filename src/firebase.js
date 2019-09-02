0cb58f59b7c3f34f470556a747b153ba8c0a1fc0
omchiii <53796452+omchiii@users.noreply.github.com>
2019-09-02 01:26:28 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import * as firebase from 'firebase';
 
   var firebaseConfig = {
-    apiKey: "AIzaSyD-FRa1vTm4c4KsmAPCw8oiIkjYw9UJR9I",
-    authDomain: "socialmedia-1afb2.firebaseapp.com",
-    databaseURL: "https://socialmedia-1afb2.firebaseio.com",
-    projectId: "socialmedia-1afb2",
-    storageBucket: "socialmedia-1afb2.appspot.com",
-    messagingSenderId: "1091167850266",
-    appId: "1:1091167850266:web:0b4b5f31ce863a37"
+
   };
 
 
